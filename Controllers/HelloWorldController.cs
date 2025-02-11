71872e5c2a79b57e5cb73a78cd2d6b4e385ff1e1
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-11 11:55:48 +0800

    Add a controller

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -13,8 +13,9 @@ public class HelloWorldController : Controller
   }
   // 
   // GET: /HelloWorld/Welcome/ 
-  public string Welcome(string name, int numTimes = 1)
+  // GET: /HelloWorld/Welcome/3?name=Rick
+  public string Welcome(string name, int ID = 1)
   {
-    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
   }
 }
